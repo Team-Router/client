dce89382e9e9f42340868d8087be37331a558846
waterglasses <hsk10271@gmail.com>
2023-06-10 22:46:05 +0900

    refactor: kakaomap dom 접근 구현에서 next/script 태그로 리팩토링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,17 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import KaKaoMap from '@/components/KaKaoMap';
 import PointInput from '@/components/PointInput';
 
 export default function Home() {
-  const [mapLoaded, setMapLoaded] = useState(false);
   const [startPoint, setStartPoint] = useState('');
-
-  useEffect(() => {
-    const $script = document.createElement('script');
-    $script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY}&libraries=services&autoload=false`;
-    $script.addEventListener('load', () => setMapLoaded(true));
-    document.head.appendChild($script);
-  }, []);
 
   return (
     <>
       <PointInput startPoint={startPoint} />
-      {mapLoaded && <KaKaoMap setStartPoint={setStartPoint} />}
+      <KaKaoMap setStartPoint={setStartPoint} />
     </>
   );
 }
