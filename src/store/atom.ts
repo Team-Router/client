81ee2da63d6292e3a598ed85a9ba479158672a00
wogha95 <wogha95@naver.com>
2023-06-17 21:12:13 +0900

    feat: location Atom 작성

--- src/store/atom.ts
+++ src/store/atom.ts
@@ -6,3 +6,15 @@ export const addressAtom = atom<{ start: string; end: string }>({
   start: '',
   end: '',
 });
+
+export const locationAtom = atom<{
+  startLatitude: number;
+  startLongitude: number;
+  endLatitude: number;
+  endLongitude: number;
+}>({
+  startLatitude: 33.450701,
+  startLongitude: 126.570667,
+  endLatitude: 33.450701,
+  endLongitude: 126.570667,
+});
