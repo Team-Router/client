60d1b089e5942f236bfc837424f6b8b095d2e38b
waterglasses <hsk10271@gmail.com>
2023-07-17 00:59:54 +0900

    feat: 메인 페이지 탭(즐겨찾기/경로) 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,42 @@
 'use client';
+import DirectionsBikeIcon from '@mui/icons-material/DirectionsBike';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { Tab, Tabs } from '@mui/material';
+import { useState } from 'react';
 
 import AddressInput from '@/components/AddressInput';
+import Favorites from '@/components/Favorites';
 import KaKaoMap from '@/components/KaKaoMap';
 
 export default function Home() {
+  const [tabValue, setTabValue] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newTabValue: number) => {
+    setTabValue(newTabValue);
+  };
+
   return (
     <>
-      <AddressInput />
-      <KaKaoMap />
+      <Tabs
+        value={tabValue}
+        onChange={handleChange}
+        aria-label="icon-tabs"
+        variant="fullWidth"
+      >
+        <Tab
+          icon={<DirectionsBikeIcon />}
+          aria-label="routes"
+          label="경로 찾기"
+        />
+        <Tab icon={<FavoriteIcon />} aria-label="favorite" label="즐겨찾기" />
+      </Tabs>
+      {tabValue === 0 && (
+        <>
+          <AddressInput />
+          <KaKaoMap />
+        </>
+      )}
+      {tabValue === 1 && <Favorites />}
     </>
   );
 }
