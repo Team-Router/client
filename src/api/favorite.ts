a9e2e50c4c28448c3cef8b735c7c6640a7420d1a
waterglasses <hsk10271@gmail.com>
2023-07-22 21:19:19 +0900

    feat: favorites(즐겨찾기) 관련 모든 api 구현

--- src/api/favorite.ts
+++ src/api/favorite.ts
@@ -1,7 +1,60 @@
 import { FavoriteStationResponse } from '@/types/station';
 
-import { getFetch } from './common';
+import { deleteFetch, getFetch, postFetch } from './common';
 
-export const getFavoriteAllStation = () => {
-  return getFetch('/favorite') as Promise<FavoriteStationResponse>;
+interface PlaceParam {
+  name: string;
+  longitude: number;
+  latitude: number;
+}
+
+export const getFavoriteAllStation = (token: string) => {
+  return getFetch('/favorite', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }) as Promise<FavoriteStationResponse>;
+};
+
+export const addFavoriteStation = (stationId: string, token: string) => {
+  return postFetch(`/favorite?stationId=${stationId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const deleteFavoriteStation = (stationId: string, token: string) => {
+  return deleteFetch(`/favorite?stationId=${stationId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const getAllFavoritePlace = () => {
+  return getFetch('/place');
+};
+
+export const addFavoritePlace = (
+  { name, longitude, latitude }: PlaceParam,
+  token: string
+) => {
+  return postFetch(
+    `/place`,
+    { name, longitude, latitude },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+};
+
+export const deleteFavoritePlace = (favoriteId: string, token: string) => {
+  return deleteFetch(`/place?favoriteId=${favoriteId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 };
